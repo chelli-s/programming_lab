2bbfe85cb8ee2269e9f3a38bc735d2946be4a32f
repl.it user <replituser@example.com>
2021-02-09 09:36:34 +0000

    aggiunto esame.py

--- esame.py
+++ esame.py
@@ -2,48 +2,106 @@ class ExamException(Exception):
 
         pass
 
-class Diff:
-    
-    def __init__(self,ratio=1):
+class CSVTimeSeriesFile:
 
-        if ratio==None:
-            raise ExamException('None non è supportato')
-        
-        if isinstance(ratio,str):
-            raise ExamException('Sono supportati solo numeri')
+    def __init__(self, name):
 
-        if ratio<0:
-            raise ExamException('Ratio deve essere positivo')
-        
-        if ratio==0:
-            raise ExamException('0 non e un valore supportato')
-        
-        self.ratio=ratio
-        
+        self.name = name
 
-    
-    def compute(self,lista):
-        if not isinstance(lista,list):
-            raise ExamException('Non e una lista')
-        if len(lista)<2:
-            raise ExamException('La lunghezza della lista deve essere almeno 2')
+    def get_data(self):
+
+        
         output = []
-        i=0     
-        while(i<len(lista)-1):
-            if not (isinstance(lista[i], int) or  isinstance(lista[i], float)):
-                raise ExamException('Il valore inserito non e un numero')
+        
+        try:
+            my_file = open(self.name, 'r') #apro il file con il nome scelto dall'utente 
+        except:
+            raise ExamException('impossibile aprire il file')
+            
 
-            if not (isinstance(lista[i+1], int) or  isinstance(lista[i+1], float)):
-                raise ExamException('Il valore inserito non e un numero')
+        for line in my_file:
+            elements = line.split(',') #per ogni linea nell mio file vado a splittare in n elementi quando incontro la virgola
+            
 
-            if lista[i]>lista[i+1]:
-                raise ExamException('la differenza e negativa')
-            valore=lista[i+1]-lista[i]
-            media=valore/self.ratio
-            output.append(media)
-            i=i+1
-        return output
+            if len(elements)>=2:
+                if elements[0] != 'epoch':
+                        
+                    timestamp  = elements[0] #salvo le timestamp in una variabile
+                    value = elements[1] #le temperature nell'altra
+                    
+                    try:
+                        timestamp = int(timestamp) #vado a convertire le timestamp in int
+                    except:
+                        raise ExamException('valore non numerico')
+                    
+                    
+                    if timestamp<0:
+                        raise ExamException('valore negativo non accettato')
 
-diff = Diff()
-result = diff.compute([2,4,8,16])
-print(result) # Deve stampare a schermo [2,4,8]
+                    try:
+                        value = float(value) #e le temperature in float
+                    except:
+                        raise ExamException('valore non numerico')
+                     
+                    values = [timestamp,value] #salvo le variabili in un vettore
+                    
+                    output.append(values) #salvo questo vettore in un altro vettore in modo da creare un array bidimensionale
+            else:
+                print('\nin questa linea mancano elementi\n')
+                
+        for i in range(0, len(output)-1):
+            if output[i][0]>=output[i+1][0]:
+                raise ExamException('Le date sono ordinate, è stato rivelato un errore di ordine')
+        my_file.close()
+        return output#ritorno il vettore finale
+        
+
+def daily_stats(self):
+
+    if not isinstance(self,list):
+        raise ExamException('L elemento inserito non è una lista')
+
+    values = []#values è il vettore in cui salvo i valori giornalieri
+    output = []#output serve per salvare le statistiche giornaliere
+        
+    for i in range(0,len(self)-1):
+        
+        epoch = self[i][0]#salvo in una variabile 
+        day_start_epoch = epoch - (epoch % 86400)#salvo in una variabile  l’inizio di un giorno dato un timestamp epoch
+        data = []#inizializzo il vettore data, qui salvo tutte le temperature appartenenti allo stesso giorno
+        if i == 0:#se siamo al primo giro mi salvo a priori il valore nel vettore data 
+            data.append(self[i][1])
+        if epoch != 0:
+            for j in range(i+1,len(self)):
+                timestamp = self[j][0]
+                condizione = day_start_epoch + 86400
+                if timestamp < condizione :#se siamo nello stesso giorno
+                    data.append(self[j][1])#aggiungo all'array data
+                    self[j][0] = 0#e assegno il valore 0 alla data
+        
+            values.append(data)
+    
+    for i in range(0,len(values)):
+        somma = 0
+        valore_massimo = values[i][0]
+        valore_minimo = values[i][0]
+        if len(values[i])<1:#controllo che ci sia almeno un valore per giorno
+            raise ExamException('Ogni giorno ha almeno un dato')
+        for j in range(0, len(values[i])):#valuto la giornata corrente
+            somma += values[i][j]
+            if valore_massimo<values[i][j]:
+                valore_massimo = values[i][j]
+            if valore_minimo>values[i][j]:
+                valore_minimo = values[i][j]
+        valore_medio = somma/len(values[i])#per fare la media approssimata a due cifre : round(somma/len(values[i]),2)
+        statistiche = [valore_minimo,valore_massimo,valore_medio]
+        output.append(statistiche)
+
+    return  values
+
+time_series_file = CSVTimeSeriesFile(name='data.csv')
+
+
+time_series = time_series_file.get_data()
+#print(time_series)
+print(daily_stats(time_series))
